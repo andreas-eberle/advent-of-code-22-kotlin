31d11e613905dbecb3f631bb9854887f9e1a21f6
Andreas Eberle <andreas.eberle@arconsis.com>
2022-12-30 13:55:49 +0100

    Update coordinate class

--- src/Coordinate.kt
+++ src/Coordinate.kt
@@ -11,10 +11,17 @@ data class Coordinate(val x: Int, val y: Int) {
 
     operator fun minus(other: Coordinate) = Coordinate(x - other.x, y - other.y)
     operator fun plus(value: Int) = Coordinate(x + value, y + value)
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
+
+    fun wrap(width: Int, height: Int): Coordinate = Coordinate(x.mod(width), y.mod(height))
+
+    operator fun rangeTo(o: Coordinate): List<Coordinate> = (this.y..o.y).flatMap { newY -> (this.x..o.x).map { newX -> Coordinate(newX, newY) } }
+
+    fun isIn(width: Int, height: Int) = x in 0 until width && y in 0 until height
 }
 
 
-fun List<Coordinate>.maxEach(): Coordinate = reduce { acc, coordinate -> Coordinate(max(acc.x, coordinate.x), max(acc.y, coordinate.y)) }
-fun List<Coordinate>.minEach(): Coordinate = reduce { acc, coordinate -> Coordinate(min(acc.x, coordinate.x), min(acc.y, coordinate.y)) }
+fun Collection<Coordinate>.maxEach(): Coordinate = reduce { acc, coordinate -> Coordinate(max(acc.x, coordinate.x), max(acc.y, coordinate.y)) }
+fun Collection<Coordinate>.minEach(): Coordinate = reduce { acc, coordinate -> Coordinate(min(acc.x, coordinate.x), min(acc.y, coordinate.y)) }
 
 fun List<Pair<Coordinate, Coordinate>>.flatten(): List<Coordinate> = flatMap { listOf(it.first, it.second) }
